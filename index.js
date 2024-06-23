ad59992f9a2ff99257196e50d1a3158268bbccc8
pratham9467 <76515135+pratham9467@users.noreply.github.com>
2024-06-24 04:28:38 +0530

    file

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-// function cursoreffect() {
-//   let main = document.querySelector("main");
-//   let playReel = document.querySelector(".playReel");
-//
-//   main.addEventListener("mousemove", function (e) {
-//     gsap.to(playReel, {
-//       x: e.x,
-//       y: e.y,
-//     });
-//   });
-//   main.addEventListener("mouseenter", function () {
-//     gsap.to(playReel, {
-//       scale: 1,
-//       opacity: 1,
-//     });
-//   });
-//
-//   main.addEventListener("mouseleave", function () {
-//     gsap.to(playReel, {
-//       scale: 0,
-//       opacity: 0,
-//     });
-//   });
-// }
-//
-// cursoreffect();
+function cursoreffect() {
+  let main = document.querySelector("main");
+  let playReel = document.querySelector(".playReel");
 
+  main.addEventListener("mousemove", function (e) {
+    gsap.to(playReel, {
+      x: e.x,
+      y: e.y,
+    });
+  });
+  main.addEventListener("mouseenter", function () {
+    gsap.to(playReel, {
+      scale: 1,
+      opacity: 1,
+    });
+  });
 
+  main.addEventListener("mouseleave", function () {
+    gsap.to(playReel, {
+      scale: 0,
+      opacity: 0,
+    });
+  });
+}
+
+cursoreffect();
